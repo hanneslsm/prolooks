a116263b29f247d7c722f9a1331aeabf5aba601c
hanneslsm <HannesLeismann@gmail.com>
2024-06-07 16:41:10 +0200

    Add patterns

---
+++ patterns/content-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Content 1
+ * Slug: prolooks/content-1
+ * Categories: Content
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"metadata":{"bindings":{"__default":{"source":"core/pattern-overrides"}},"name":"Subheading"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|brand-3"}}}},"textColor":"brand-3"} -->
+<p class="has-brand-3-color has-text-color has-link-color">WordPress ipsum, dolor theme amet</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1,"metadata":{"bindings":{"__default":{"source":"core/pattern-overrides"}},"name":"Heading"}} -->
+<h1 class="wp-block-heading">ProLooks ipsum tortor ac accumsan condimentum, theme customizer viverra nec.</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"metadata":{"bindings":{"__default":{"source":"core/pattern-overrides"}},"name":"Intro"},"fontSize":"large"} -->
+<p class="has-large-font-size">WordPress ipsum, dolor theme amet, customizer vel fermentum, widgets nec sagittis gravida, post types et taxonomies fermentum. Morbi menu customizer, API calls sed viverra ipsum nunc aliquet bibendum. Enim, facilisis gravida neque convallis a cras semper auctor. Gutenberg blocks lorem, ac metus maximus, consectetur adipiscing elit. Theme customizer, plugin repository lorem, widget areas et, shortcode integrations vitae, arcu.</p>
+<!-- /wp:paragraph -->
