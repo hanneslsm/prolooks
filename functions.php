225d3b50348c575077fada1edca856277736b5b8
hanneslsm <HannesLeismann@gmail.com>
2024-06-10 14:43:40 +0200

    Add functions.php & custom scripts

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * prolooks functions and definitions
+ * 
+ * @package prolooks
+ * @since 1.0
+ */
+
+
+// Setup
+require get_template_directory() . '/inc/setup.php';
+
+// Enqueue files
+require get_template_directory() . '/inc/enqueuing.php';
+
+// Dashboard Widget
+require get_template_directory() . '/inc/dashboard-widget.php';
+
+/**
+ * Development only!
+ */
+// Remove default CSS variables
+require get_template_directory() . '/inc/remove-defaults.php';
