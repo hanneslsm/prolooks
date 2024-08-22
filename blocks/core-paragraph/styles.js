ab3d589643730fd5e1191ab1bc3533a50c6ed039
hanneslsm <HannesLeismann@gmail.com>
2024-08-22 15:27:55 +0200

    Add Paragraph variation "Kicker"

---
+++ blocks/core-paragraph/styles.js
@@ -0,0 +1,11 @@
+/** Page List
+ * @since ProLooks 8
+ * --------------------------------------------- */
+
+/**
+ * Register block styles
+ */
+wp.blocks.registerBlockStyle("core/paragraph", {
+  name: "paragraph-kicker",
+  label: "Kicker",
+});
