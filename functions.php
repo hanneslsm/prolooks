1609f0c7ff562e122b95bf5c74fb53cb82dce4f5
hanneslsm <HannesLeismann@gmail.com>
2025-04-02 16:26:14 +0200

    fix error

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 require get_template_directory() . '/inc/setup.php';
 
 // Patterns Setup
-require get_template_directory() . '/inc/patterns.php';
+require get_template_directory() . '/inc/block-patterns.php';
 
 // Enqueue files
 require get_template_directory() . '/inc/enqueuing.php';
