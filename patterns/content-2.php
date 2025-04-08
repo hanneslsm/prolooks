73716f01ab167f51e804b9fac77644ffe5fef5e5
hanneslsm <HannesLeismann@gmail.com>
2025-04-08 11:14:43 +0200

    Update parts

---
+++ patterns/content-2.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Content 2
+ * Slug: prolooks/content-2
+ * Categories: Content
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"metadata":{"bindings":{"__default":{"source":"core/pattern-overrides"}},"name":"Subheading"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|brand-3"}}}},"textColor":"brand-3"} -->
+<p class="has-brand-3-color has-text-color has-link-color"><?php esc_html_e('WordPress ipsum, dolor theme amet', 'prolooks');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"metadata":{"bindings":{"__default":{"source":"core/pattern-overrides"}},"name":"Heading"}} -->
+<h2 class="wp-block-heading"><?php esc_html_e('ProLooks ipsum tortor ac accumsan condimentum, theme customizer viverra nec.', 'prolooks');?></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"metadata":{"bindings":{"__default":{"source":"core/pattern-overrides"}},"name":"Intro"}} -->
+<p><?php esc_html_e('WordPress ipsum, dolor theme amet, customizer vel fermentum, widgets nec sagittis gravida, post types et taxonomies fermentum. Morbi menu customizer, API calls sed viverra ipsum nunc aliquet bibendum. Enim, facilisis gravida neque convallis a cras semper auctor. Gutenberg blocks lorem, ac metus maximus, consectetur adipiscing elit. Theme customizer, plugin repository lorem, widget areas et, shortcode integrations vitae, arcu.', 'prolooks');?></p>
+<!-- /wp:paragraph -->
